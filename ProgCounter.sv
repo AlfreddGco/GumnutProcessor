5a4956cd84bdaaf0e514e466a9c2f8a481975357
Alfredo Garcia <alfredo.garcia@toptal.com>
2021-03-14 11:32:17 -0600

    Added program counter

---
+++ ProgCounter.sv
@@ -0,0 +1,17 @@
+module ProgCounter(
+  input logic [11:0] PC_i,//address
+  input logic we,
+  input logic clk,
+  input logic cen,
+  input logic rst,
+  output logic [11:0] PC_o//address
+);
+
+  logic gclk = clk & cen;
+
+  always_ff @(posedge gclk) begin
+    if(rst) PC_o <= 12'b0;
+    else PC_o <= PC_i;
+  end
+
+endmodule
